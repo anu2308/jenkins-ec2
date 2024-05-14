327ca4d6675f79329be24d4ed796584e92bcbe68
Anushree Prasad Naik <71065830+anu2308@users.noreply.github.com>
2024-05-14 09:38:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ua-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   tags = {
